cad68a5770a72f055a5c28a981ee8f7e6a790852
YACBHK <yacine.bahaka.pro@gmail.com>
2025-06-13 15:24:51 +0200

    EventView

---
+++ Marengo/ExtractedViews/Event/SearchBarEventExtractedView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarEventExtractedView.swift
+//  Marengo
+//
+//  Created by YacineBahaka  on 13/06/2025.
+//
+
+import SwiftUI
+
+struct SearchBarEventExtractedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarEventExtractedView()
+}
